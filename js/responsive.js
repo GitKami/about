487e7658c24ccfaa645eb7d8712fd86df4a8d890
gitkami <kmd1156@wmich.edu>
2019-02-12 23:41:49 -0500

    fixed redirects on links, added back to top

--- js/responsive.js
+++ js/responsive.js
@@ -1,80 +1,38 @@
-/*Read more buttons
-window.onload = function() {
-  var dots1 = document.getElementById("dots1"),
-			dots2 = document.getElementById("dots2");
-
-	var more1 = document.getElementById("more1"),
-			more2 = document.getElementById("more2");
-	
-	var btn1 = document.getElementById("btn1"),
-			btn2 = document.getElementById("btn2");
-	var ifm	 = document.getElementById("iframe");	
-
-
-	btn1.onclick= function(){
-		if(dots1.style.display === "none"){
-			ifm.style.display = "none";
-			dots1.style.display = "inline";
-			btn1.innerHTML = "Read more";
-			more1.style.display = "none";
-		} else {
-			ifm.style.display = "block";
-			dots1.style.display = "none";
-			btn1.innerHTML = "Read less";
-			more1.style.display = "block";
-		}
-	};
-	
-	btn2.onclick= function(){
-		if(dots2.style.display === "none"){
-			dots2.style.display = "inline";
-			btn2.innerHTML = "Read more";
-			more2.style.display = "none";
-		} else {
-			dots2.style.display = "none";
-			btn2.innerHTML = "Read less";
-			more2.style.display = "block";
-		}
-	};
-};
-*/
 /* Listen for clicks in #openNav and #closeNav */
-document.addEventListener('click', function (event) {
-	if (event.target.matches('#openNav')){
+document.addEventListener("click", function (event) {
+	if (event.target.matches("#openNav")){
 		var intF = window.innerWidth;    
 		document.getElementById("about").style.width = "300px";    
 		document.getElementById("main").style.marginLeft = "300px";
 		if(intF< 450) { document.getElementById("summary").style.display = "none";}    
  
 	}
-	if (event.target.matches('#closeNav')){
+	if (event.target.matches("#closeNav")){
 		document.getElementById("summary").removeAttribute("style");    
 		document.getElementById("about").style.width = "0";    
 		document.getElementById("main").style.marginLeft= "0";    
 
 	}
 	if (event.target.matches("#j1")){
-		document.querySelector('#executive').scrollIntoView({ 
-			behavior: 'smooth' 
+		document.querySelector("#executive").scrollIntoView({ 
+			behavior: "smooth" 
 		});
 	}
 	if (event.target.matches("#j2")){
-		document.querySelector('#skills').scrollIntoView({ 
-			behavior: 'smooth' 
+		document.querySelector("#skills").scrollIntoView({ 
+			behavior: "smooth" 
 		});
 	}
 	if (event.target.matches("#j3")){
-		document.querySelector('#work').scrollIntoView({ 
-			behavior: 'smooth' 
+		document.querySelector("#work").scrollIntoView({ 
+			behavior: "smooth" 
 		});
 	}
 	if (event.target.matches("#j4")){
-		document.querySelector('#projects').scrollIntoView({ 
-			behavior: 'smooth' 
+		document.querySelector("#executive").scrollIntoView({ 
+			behavior: "smooth" 
 		});
 	}
 
-	// Don't follow the link
-	event.preventDefault();
-}, false);
+});
 
